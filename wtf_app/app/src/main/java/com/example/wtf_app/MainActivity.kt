1b2898c191a7b34c6afdb1e1c9fd40fe0559167f
10Dev <today13113@gmail.com>
2022-08-03 04:23:00 +0700

    update feature pick-image

--- wtf_app/app/src/main/java/com/example/wtf_app/MainActivity.kt
+++ wtf_app/app/src/main/java/com/example/wtf_app/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.wtf_app
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.wtf_app.databinding.ActivityMainBinding
 
 
+const val REQUEST_CODE = 100
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityMainBinding
 
@@ -17,12 +20,30 @@ class MainActivity : AppCompatActivity() {
 
         // Set up the listeners for take photo and video capture buttons
         viewBinding.btnCamera.setOnClickListener { openCamera() }
+        viewBinding.btnPick.setOnClickListener { openImageGallery() }
     }
-    private  fun openCamera(){
+
+    private fun openCamera() {
         val intent = Intent(
             this,
             CameraActivity::class.java
         )
         startActivity(intent)
     }
+
+    private fun openImageGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, REQUEST_CODE)
+    }
+
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE) {
+            viewBinding.imgPreview.setImageURI(data?.data) // handle chosen image
+        }
+    }
+
 }
